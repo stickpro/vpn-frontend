209bb55f1a77ef7e14274981dce83daff850670e
Vlad Bulgakov <stick.qwe@gmail.com>
2023-03-20 01:51:19 +0300

    remove button code

--- pages/auth.vue
+++ pages/auth.vue
@@ -39,12 +39,6 @@
           сказанный роботом</label
         >
         <ControlsVOtp :digit-count="4" @update:otp="code = $event" />
-        <button
-          @click="auth"
-          class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        >
-          Получить код
-        </button>
       </div>
     </div>
   </div>
